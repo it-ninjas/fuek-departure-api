aebaa761494d772812d36b88c8a4c7a2560b441c
Mountain Star <github@mtnstar.net>
2023-04-25 11:34:02 +0200

    Implement first test for login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,15 @@
 //it('POST login with correct credentials returns valid JWT token', () => {
-  //cy.request('POST', '/login').should((response) => {
-    //expect(response.status).to.eq(200);
-  //});
+//cy.request('POST', '/login').should((response) => {
+//expect(response.status).to.eq(200);
+//});
 //});
 
 it("POST login returns 'Email and password required' if no params", () => {
-  cy.request('POST', '/login').should((response) => {
+  cy.request({
+    method: 'POST',
+    url: '/api/login',
+    failOnStatusCode: false,
+  }).should((response) => {
     expect(response.status).to.eq(400);
   });
 });
--- server.js
+++ server.js
@@ -1,12 +1,48 @@
 import debug from 'debug';
 import http from 'http';
-import app from '../app.js';
+import createError from 'http-errors';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import { indexRoutes } from './routes/index.js';
+import { loginRoutes } from './routes/login.js';
+
+const app = express();
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRoutes);
+app.use('/api/', loginRoutes);
+
+// catch 404 and forward to error handler
+app.use(function (_req, _res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, _next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '4242');
+let port = normalizePort(process.env.PORT || '4242');
 app.set('port', port);
 process.title = process.env.TITLE || 'departureApiDev';
 
@@ -29,7 +65,7 @@ server.on('listening', onListening);
  */
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+  let port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
@@ -53,7 +89,7 @@ function onError(error) {
     throw error;
   }
 
-  var bind = typeof port === 'string'
+  let bind = typeof port === 'string'
     ? 'Pipe ' + port
     : 'Port ' + port;
 
@@ -77,8 +113,8 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
+  let addr = server.address();
+  let bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
